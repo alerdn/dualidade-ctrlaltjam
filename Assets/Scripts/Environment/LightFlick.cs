70bd55d1d81a9526c9e8afd7fcb8484b95cfde83
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2023-05-21 17:14:50 -0400

    feat: luzes ambientes animadas

---
+++ Assets/Scripts/Environment/LightFlick.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using DG.Tweening;
+
+public class LightFlick : MonoBehaviour
+{
+    [SerializeField] private float _minInterval = 3f;
+    [SerializeField] private float _maxInterval = 5f;
+
+    private Light2D _light;
+
+    private void Start()
+    {
+        _light = GetComponent<Light2D>();
+        StartCoroutine(LightRoutine());
+    }
+
+    private IEnumerator LightRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(_minInterval, _maxInterval));
+
+            yield return DOTween
+                .To(() => _light.intensity, (intensity) => _light.intensity = intensity, 0.4f, .05f)
+                .SetLoops(Random.Range(1, 4) * 2, LoopType.Yoyo)
+                .WaitForCompletion();
+        }
+    }
+}
