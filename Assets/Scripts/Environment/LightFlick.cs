e63ce33da6fa4d8f4624b666ae8f08d5d154a44e
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2023-05-21 18:01:50 -0400

    feat: URP configurado

--- Assets/Scripts/Environment/LightFlick.cs
+++ Assets/Scripts/Environment/LightFlick.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 
 public class LightFlick : MonoBehaviour
 {
+    [SerializeField] [Range(0f, 1f)] private float _lowerPercentage = .8f;
     [SerializeField] private float _minInterval = 3f;
     [SerializeField] private float _maxInterval = 5f;
 
@@ -24,7 +25,7 @@ public class LightFlick : MonoBehaviour
             yield return new WaitForSeconds(Random.Range(_minInterval, _maxInterval));
 
             yield return DOTween
-                .To(() => _light.intensity, (intensity) => _light.intensity = intensity, 0.4f, .05f)
+                .To(() => _light.intensity, (intensity) => _light.intensity = intensity, _light.intensity * _lowerPercentage, .05f)
                 .SetLoops(Random.Range(1, 4) * 2, LoopType.Yoyo)
                 .WaitForCompletion();
         }
