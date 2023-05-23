d611b14182d239a206c67965e211a5e428513f04
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2023-05-23 19:11:23 -0400

    feat: apresentação do ato

---
+++ Assets/Scripts/Act/Act.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = " New Act")]
+public class Act : ScriptableObject
+{
+    public string Title;
+    public string Description;
+}
