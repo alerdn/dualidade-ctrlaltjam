64d9a925da4ee0a596db5ea44de0a0febcac3665
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2023-05-27 02:44:37 -0400

    feat: icone das habilidades criados

--- Assets/Scripts/UI/Menu/Ability/AbilityDetail.cs
+++ Assets/Scripts/UI/Menu/Ability/AbilityDetail.cs
@@ -10,12 +10,27 @@ public class AbilityDetail : MonoBehaviour
     [SerializeField] private TMP_Text _title;
     [SerializeField] private Image _icon;
     [SerializeField] private TMP_Text _description;
+    [SerializeField] private TMP_Text _costState;
+    [SerializeField] private Color _defaultColor;
+    [SerializeField] private Color _stealthColor;
+    [SerializeField] private Color _assassinColor;
 
     internal void Show(Ability data)
     {
         _title.text = data.Title;
         _icon.sprite = data.Icon;
         _description.text = data.Description;
+
+        if (data.State == Ability.AbilityState.LOCKED)
+        {
+            _costState.text = $"pontos requeridos: {data.Cost}";
+            _costState.color = _defaultColor;
+        }
+        else
+        {
+            _costState.text = "habilitada";
+            _costState.color = data.Type == AbilityType.STEALTH ? _stealthColor : _assassinColor;
+        }
 
         gameObject.SetActive(true);
     }
